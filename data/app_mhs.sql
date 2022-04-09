f447bc69d520a6278013092d945d4dfe01892dd1
nasruddin <nasruddin20182019@gmail.com>
2022-04-10 01:07:13 +0700

    ubah name databases

--- data/app_mhs.sql
+++ data/app_mhs.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `epiz_31456385_app_mhs`
+-- Database: `app_mhs`
 --
 
 -- --------------------------------------------------------
